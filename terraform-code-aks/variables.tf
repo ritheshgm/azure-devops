8131ecb095c95ed48ba7f84d52d640d35e997bf2
ritheshgm <50571179+ritheshgm@users.noreply.github.com>
2020-04-05 23:15:01 +0530

    Update variables.tf

--- terraform-code-aks/variables.tf
+++ terraform-code-aks/variables.tf
@@ -1,3 +1,6 @@
+variable "client_id" {}
+variable "client_secret" {}
+
 variable "agent_count" {
     default = 3
 }
@@ -19,19 +22,17 @@ variable resource_group_name {
 }
 
 variable location {
-    default = "Central US"
+    default = "Korea Central"
 }
 
 variable log_analytics_workspace_name {
     default = "testLogAnalyticsWorkspaceName"
 }
 
-# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
 variable log_analytics_workspace_location {
     default = "eastus"
 }
 
-# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
 variable log_analytics_workspace_sku {
     default = "PerGB2018"
 }
